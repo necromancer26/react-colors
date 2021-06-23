6a5fdfc9966d4eed260a018ccda1e8e4d2b65974
Khalil <khalil.chettaoui06@gmail.com>
2021-06-23 07:07:06 +0100

    style colorbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 
 function App() {
   return (
     <div>
-      <Palette {...seedColors[5]} />
+      <Palette {...seedColors[2]} />
     </div>
   );
 }
